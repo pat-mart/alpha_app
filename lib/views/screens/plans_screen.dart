768754cc54cc4f015d10508badea40961b1649c0
Patrick Martin <martinp@northshoreschools.org>
2023-07-06 22:13:10 -0400

    added headers

--- lib/views/screens/plans_screen.dart
+++ lib/views/screens/plans_screen.dart
@@ -1,9 +1,10 @@
-import 'package:astro_planner/models/plan_m.dart';
-import 'package:astro_planner/views/screens/plan_sheet.dart';
+import 'package:astro_planner/views/screens/sheet.dart';
+import 'package:astro_planner/views/smallwidgets/plan_sheet_body.dart';
 import 'package:astro_planner/views/smallwidgets/plan_v.dart';
+import 'package:flutter/cupertino.dart';
 
 import 'package:flutter/material.dart';
-import '../../viewmodels/list_vm.dart';
+import 'package:provider/provider.dart';
 import '../../viewmodels/plan_vm.dart';
 
 class PlansScreen extends StatefulWidget {
@@ -16,68 +17,50 @@ class PlansScreen extends StatefulWidget {
 class _PlansScreenState extends State<PlansScreen>{
 
   final PlanViewModel planVm = PlanViewModel();
-  final ListViewModel<Plan> listViewModel = ListViewModel();
 
-  void refresh(){
-    setState(() {});
-  }
+  void refresh() => setState(() {});
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-          child: Container(
-            margin: const EdgeInsets.only(left: 12, right: 12, top: 100),
-            child: Column(
-                children: <Widget>[
-                  Row( //Header with button
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      const Text('My Plans', style: TextStyle(fontSize: 36,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white)),
-                      IconButton(
-                        onPressed: () =>
-                          setState(() {
-                            showModalBottomSheet(
-                              backgroundColor: const Color(0xFF2E2E2E),
-                              context: context,
-                              isDismissible: false,
-                              barrierColor: const Color(0x67000000),
-                              shape: const RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.vertical(top: Radius.circular(16))
-                              ),
-                              builder: (BuildContext context) => Wrap(
-                                children: <Widget>[
-                                  PlanSheet(onAddPlan: () => setState(() {
-                                    refresh();
-                                  }))
-                                ])
-                            );
-                          }),
-                        style: const ButtonStyle(
-                            backgroundColor: MaterialStatePropertyAll<Color>(
-                                Colors.white)),
-                        padding: EdgeInsets.zero,
-                        icon: const Icon(
-                            Icons.add_circle_outline, color: Colors.white,
-                            size: 36),
-                      )
-                    ],
-                  ),
-                  ListView.builder(
-                      physics: const NeverScrollableScrollPhysics(),
-                      shrinkWrap: true,
-                      itemCount: planVm.planList.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return PlanCard(
-                            plan: planVm.planList.elementAt(index),
-                            onDelete: () => setState(() => planVm.removeObject(index))
-                        );
-                      }
-                  )
-                ]
+
+    return CupertinoPageScaffold(
+      child: CustomScrollView(
+        slivers: <Widget>[
+          CupertinoSliverNavigationBar(
+            backgroundColor: CupertinoColors.black,
+            padding: EdgeInsetsDirectional.zero,
+            largeTitle: const Text('My plans'),
+            leading: const Icon(Icons.add_circle_outline),
+            trailing: IconButton(
+              icon: const Icon(CupertinoIcons.add_circled, size: 36),
+              onPressed: () {
+                setState(() {
+                  showCupertinoModalPopup(
+                    context: context,
+                    barrierDismissible: false,
+                    barrierColor: const Color(0xBB000000),
+                    builder: (BuildContext context) {
+                      return Sheet(child: PlanSheet(onAddPlan: refresh));
+                    }
+                  );
+                });
+              },
             ),
-          )
-      );
+          ),
+
+          SliverFillRemaining(
+            child: ListView.builder(
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: planVm.modelList.length,
+              itemBuilder: (BuildContext context, int index) {
+                return PlanCard(
+                  plan: planVm.modelList.elementAt(index),
+                  onDelete: () => setState(() => planVm.removeModelAt(index))
+                );
+              }
+            ),
+          )]
+      ),
+    );
   }
 }
